407420e2f053ab44d3d5f936a7a9e658712f19e6
piragna1 <gonzalovarelaalagna@gmail.com>
2024-06-01 15:41:31 -0300

    Desarrollo
    Crud agenda

--- proyectoJsonExcepciones/src/main/java/com/gchaldu/agenda/model/entity/Agenda.java
+++ proyectoJsonExcepciones/src/main/java/com/gchaldu/agenda/model/entity/Agenda.java
@@ -1,6 +1,29 @@
 package com.gchaldu.agenda.model.entity;
 import com.gchaldu.contacto.model.entity.Contacto;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class Agenda {
-    private List<Contacto> list;
+    private Map<String, Contacto> map;
+
+    public Agenda() {
+        this.map=new HashMap<>();
+    }
+
+    public void agregarContacto(Contacto contacto){
+        this.map.put(contacto.getTelefono(), contacto);
+    }
+
+    public Contacto leerContacto(String telefono){
+        return  this.map.get(telefono);
+    }
+
+    public void actualizarContacto(String telefono, Contacto contacto){
+        this.map.replace(telefono, contacto);
+    }
+    public Contacto removerContacto(String telefono){
+        return this.map.remove(telefono);
+    }
 }
--- proyectoJsonExcepciones/src/main/java/com/gchaldu/agenda/view/ViewAgenda.java
+++ proyectoJsonExcepciones/src/main/java/com/gchaldu/agenda/view/ViewAgenda.java
@@ -1,4 +1,15 @@
 package com.gchaldu.agenda.view;
 
+import com.gchaldu.agenda.model.entity.Agenda;
+
+import java.util.Scanner;
+
 public class ViewAgenda {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public ViewAgenda() {
+    }
+    public Agenda generarAgenda(){
+        return new Agenda();
+    }
 }
