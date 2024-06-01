276208e00596c129566e1a29bd82919bc6e183ad
piragna1 <gonzalovarelaalagna@gmail.com>
2024-06-01 14:18:43 -0300

    Desarrollo de clases
    Agenda
    Contacto

--- proyectoJsonExcepciones/src/main/java/com/gchaldu/agenda/model/entity/Agenda.java
+++ proyectoJsonExcepciones/src/main/java/com/gchaldu/agenda/model/entity/Agenda.java
@@ -1,4 +1,11 @@
 package com.gchaldu.agenda.model.entity;
 
+import com.gchaldu.contacto.model.entity.Contacto;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.awt.*;
+import java.util.List;
+
 public class Agenda {
+    private List<Contacto> list;
 }
